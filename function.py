308fba3dc4546e8e65f02da4f7f79b98ad78e94c
vjbytes101 <vj.yadav91@yahoo.com>
2017-01-09 20:20:30 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,1 +1,13 @@
+#def sayHelloo():
+#    print "Hello World"
+#print "he is offline"
+#sayHelloo()
 
+def printMax(a,b):
+    if a > b:
+        print a,"is maximum than",b
+    else:
+        print b,"is maximum than",a
+
+printMax(3,4)
+printMax(4,3)
